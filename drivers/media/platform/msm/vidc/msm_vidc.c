82c297fc85842a1b3182de1827f55c56c14c835c
duckyduckG <duckyduckg65@gmail.com>
2025-09-06 14:49:39 +0500

    Revert "msm: vidc: Switch to timer_setup()"
    This reverts commit b5f47203d2fd76ee6f11f04b542fdbe0c71e86c5.

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1870,9 +1870,9 @@ static const struct v4l2_ctrl_ops msm_vidc_ctrl_ops = {
 	.g_volatile_ctrl = msm_vidc_op_g_volatile_ctrl,
 };
 
-static void batch_timer_callback(struct timer_list *t)
+static void batch_timer_callback(unsigned long data)
 {
-	struct msm_vidc_inst *inst = from_timer(inst, t, batch_timer);
+	struct msm_vidc_inst *inst = (struct msm_vidc_inst *)data;
 
 	if (!inst->batch.enable)
 		return;
@@ -2017,8 +2017,8 @@ void *msm_vidc_open(int core_id, int session_type)
 	}
 
 	INIT_WORK(&inst->batch_work, msm_vidc_batch_handler);
-	timer_setup(&inst->batch_timer,
-				batch_timer_callback, 0);
+	setup_timer(&inst->batch_timer,
+				batch_timer_callback, (unsigned long)inst);
 
 	return inst;
 fail_init:
