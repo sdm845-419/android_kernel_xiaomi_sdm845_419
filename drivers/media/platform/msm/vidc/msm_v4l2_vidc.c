40d2d85114b96c5ca707300775e2641a9c54a5b3
duckyduckG <duckyduckg65@gmail.com>
2025-09-06 14:49:39 +0500

    Revert "msm: vidc: Remove boot markers"
    This reverts commit 0fd075f25fbf24b5fc87d4e33230063fa3d8845a.

--- drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
+++ drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -533,6 +533,8 @@ static int msm_vidc_probe_vidc_device(struct platform_device *pdev)
 	struct device *dev;
 	int nr = BASE_DEVICE_NUMBER;
 
+	place_marker("M - DRIVER Video Start");
+
 	if (!vidc_driver) {
 		dprintk(VIDC_ERR, "Invalid vidc driver\n");
 		return -EINVAL;
@@ -639,6 +641,7 @@ static int msm_vidc_probe_vidc_device(struct platform_device *pdev)
 		goto err_fail_sub_device_probe;
 	}
 
+	place_marker("M - DRIVER Video Ready");
 	return rc;
 
 err_fail_sub_device_probe:
@@ -778,6 +781,7 @@ static int msm_vidc_pm_suspend(struct device *dev)
 
 static int msm_vidc_pm_resume(struct device *dev)
 {
+	update_marker("vidc resumed");
 	dprintk(VIDC_INFO, "%s\n", __func__);
 	return 0;
 }
@@ -846,8 +850,13 @@ static int msm_vidc_pm_freeze(struct device *dev)
 	if (!core)
 		return 0;
 
-	if (of_device_is_compatible(dev->of_node, "qcom,msm-vidc"))
+	if (of_device_is_compatible(dev->of_node, "qcom,msm-vidc")) {
+		place_marker("vidc hibernation start");
+
 		rc = msm_vidc_freeze_core(core);
+
+		place_marker("vidc hibernation end");
+	}
 
 	dprintk(VIDC_INFO, "%s: done\n", __func__);
 
