498b6cba4c776e6419fb02a2bb83c0a3b98141a8
duckyduckG <duckyduckg65@gmail.com>
2025-09-06 14:49:39 +0500

    Revert "msm: vidc: Remove deprecated iommu initialization apis"
    This reverts commit 9f21b11fc96b4dfe9fa3bbe585af755a3ac9169a.

--- drivers/media/platform/msm/vidc/msm_vidc_resources.h
+++ drivers/media/platform/msm/vidc/msm_vidc_resources.h
@@ -61,7 +61,7 @@ struct context_bank_info {
 	bool is_secure;
 	struct addr_range addr_range;
 	struct device *dev;
-	struct iommu_domain *domain;
+	struct dma_iommu_mapping *mapping;
 };
 
 struct buffer_usage_table {
